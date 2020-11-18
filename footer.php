4fcd3acb366028bca143f2d8719be377648d70c6
chowdigital <sean@chowdigital.com>
2020-11-18 11:42:57 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package Wordpress-bootstrap-starter-theme
+ */
+
+?>
+<!-- Footer -->
+<footer class="page-footer font-small dark-background  pt-4">
+
+
+  <!-- Footer Links -->
+  <div class="container text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-lg-6 mt-lg-0 mt-3 white-content-box">
+
+        <!-- Content -->
+        
+        <!--Section heading-->
+    <h2 class="h1-responsive font-weight-bold my-4">SIGN UP TO OUR NEWSLETTER</h2>
+    <!--Section description-->
+    <p class="col-md-11">News and special events</p>
+
+    <div class="row">
+
+
+
+    <!-- Begin Mailchimp Signup Form -->
+<div id="mc_embed_signup">
+<form action="https://blackbooksoho.us17.list-manage.com/subscribe/post?u=64ab03f3af339dfeb72f5e5fe&amp;id=12fa2e0fcf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+      <div class="row">
+            <div class="md-form mb-0">
+           
+          <input type="email" value="" name="EMAIL" class="required email form-control" id="mce-EMAIL">
+          <label for="mce-EMAIL" class="mdbInput fc-wide">Email <span class="asterisk">*</span></label>
+          </div>
+      </div>
+<div class="row">
+  <div class="col-md-6">
+        <div class="md-form mb-0">
+        <input type="text" value="" name="FNAME" class="required form-control" id="mce-FNAME">
+              <label for="mce-FNAME" class="mdbInput">First Name  <span class="asterisk">*</span></label>
+           
+            </div>
+  </div>
+  <div class="col-md-6">
+        <div class="md-form mb-0">
+       
+        <input type="text" value="" name="LNAME" class="form-control" id="mce-LNAME">
+        <label for="mce-LNAME"  class="mdbInput">Last Name </label>
+      </div>
+  </div>
+</div>
+
+<div class="row">
+  
+<div class="md-form mb-0">
+
+        
+  <input type="text" name="PHONE" class="form-control" value="" id="mce-PHONE">
+  <label for="mce-PHONE" class="mdbInput fc-wide">Phone Number </label>
+</div>
+</div>
+
+
+
+
+
+
+
+  <div class="">
+
+    <div class="row datefield">
+      <div class="col-md-6">
+      <div class="md-form mb-0">
+
+        <input class="birthday " type="text" pattern="[0-9]*" value="" size="2" maxlength="2" name="BIRTHDAY[day]" id="mce-BIRTHDAY-day" class="form-control">
+        <label for="mce-BIRTHDAY-day" class="mdbInput">DD </label>
+  </div>
+    </div>
+  <div class="col-md-6">
+  <div class="md-form mb-0">
+
+      <input class="birthday " type="text" pattern="[0-9]*" value="" size="2" maxlength="2" name="BIRTHDAY[month]" id="mce-BIRTHDAY-month" class="form-control">
+      <label for="mce-BIRTHDAY-month" class="mdbInput">MM</label>
+  </div>
+  </div>
+  <p class="">BIRTHDAY</p>
+    </div>
+  </div>	<div id="mce-responses" class="clear">
+      <div class="response" id="mce-error-response" style="display:none"></div>
+      <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+      <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_64ab03f3af339dfeb72f5e5fe_12fa2e0fcf" tabindex="-1" value=""></div>
+      <div class="clear">
+        <input type="submit" value="Send" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+      </div>
+  </form>
+  </div>
+
+    <!--End mc_embed_signup-->
+
+
+
+
+        
+
+    </div>
+
+
+    <!--Section: Contact v.2-->
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-sm-6 col-lg-2 offset-lg-1">
+
+        <!-- Links -->
+        <h2 class="h3-responsive font-weight-bold my-4">OPENING TIMES</h2>
+    <!--Section description-->
+    <p class="no-line-break">Monday: <?php echo get_theme_mod('monday', 'Closed'); ?><p>
+    <p class="no-line-break">Tuesday: <?php echo get_theme_mod('tuesday', 'Closed'); ?><p>
+    <p class="no-line-break">Wednesday:  <?php echo get_theme_mod('wednesday', 'Closed'); ?><p>
+    <p class="no-line-break">Thursday: <?php echo get_theme_mod('thursday', 'Closed'); ?><p>
+    <p class="no-line-break">Friday: <?php echo get_theme_mod('friday', 'Closed'); ?><p>
+    <p class="no-line-break">Saturday <?php echo get_theme_mod('saturday', 'Closed'); ?><p>
+    <p class="no-line-break">Sunday: <?php echo get_theme_mod('sunday', 'Closed'); ?><p>
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-sm-6 col-lg-3">
+
+        <!-- Links -->
+        <h2 class="h3-responsive font-weight-bold my-4">BLACK BOOK</h2>
+    <!--Section description-->
+   
+  
+
+
+
+
+   
+        <ul class="list-unstyled">
+          <li>
+          <i class="fa fa-home" aria-hidden="true"></i>&nbsp;&nbsp;23 Frith Street, Soho 
+          </li>
+          <li>
+              <a href="https://www.google.com/maps/place/TRADE+Soho/@51.5133559,-0.1333751,17z/data=!3m1!4b1!4m5!3m4!1s0x48760529ea75a7b3:0x89287c620f263591!8m2!3d51.5133559!4d-0.1311864?shorturl=1"><i class="fas fa-map-marked-alt mt-4"></i>&nbsp;&nbsp;View on Google Maps </a>
+          </li>
+          <li>
+          <a href="tel:+442074341724"><i class="fas fa-phone mt-4"></i>&nbsp;&nbsp;020 7434 1724 </a>
+          </li>
+          <li>
+          <a href="mailto:info@blackbooksoho.co.uk"><i class="fas fa-envelope mt-4"></i>&nbsp;&nbsp;info@blackbooksoho.co.uk</a>
+          </li>
+          <li>
+          <a href="http://blackbooksoho.co.uk/careers"><i class="fa fa-rocket mt-4" aria-hidden="true"></i>&nbsp;&nbsp;Careers</a> 
+          </li>
+          <li>
+          <a href="http://blackbooksoho.co.uk/privacy-policy"><i class="fa fa-lock mt-4" aria-hidden="true"></i>&nbsp;&nbsp;Privacy Policy</a>
+          </li>
+          <li>  
+          <a href="https://www.facebook.com/The-Black-Book-Soho-220715369254758"><i class="fab fa-facebook-square fa-2x mt-4"></i> &nbsp;&nbsp;</a>
+          <a href="https://twitter.com/blackbooksoho"><i class="fab fa-twitter fa-2x mt-4"></i> &nbsp;&nbsp;</a>
+          <a href="https://www.instagram.com/theblackbooksoho/"><i class="fab fa-instagram fa-2x mt-4"></i> &nbsp;&nbsp;</a>
+          </li>
+          
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="text-center py-3 black-background">A 
+    <a href="https://twitter.com/blackbooksoho"> Cloudsdale</a> Website
+  </div>
+  <!-- Copyright -->
+
+</footer>
+
+
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
